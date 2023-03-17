cba98bb70f3514bb7298e0020671d34fa20b5b0d
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-03-17 13:21:53 +0000

    debuggin with binding irb

--- Phase-Two-Skill-Challenges/lib/letter_counter.rb
+++ Phase-Two-Skill-Challenges/lib/letter_counter.rb
@@ -1,21 +1,24 @@
 # File: letter_counter.rb
 
+# File: letter_counter.rb
+
 class LetterCounter
   def initialize(text)
     @text = text
   end
 
   def calculate_most_common()
-    counter = Hash.new(1)
+    counter = Hash.new(0)
     most_common = nil
     most_common_count = 1
     @text.chars.each do |char|
       next unless is_letter?(char)
-      counter[char] = (counter[char] || 1) + 1
+      counter[char] = (counter[char] || 0) + 1
       if counter[char] > most_common_count
         most_common = char
-        most_common_count += counter[char]
+        most_common_count = counter[char]
       end
+      # binding.irb (uncomment to use debugger)
     end
     return [most_common_count, most_common]
   end
