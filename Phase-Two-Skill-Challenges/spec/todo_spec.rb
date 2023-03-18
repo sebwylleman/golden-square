eb295d45aeddc25a8c7ed00f8d50d6a4b7e9b128
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-03-18 08:22:52 +0000

    todo_spec file done

--- Phase-Two-Skill-Challenges/spec/todo_spec.rb
+++ Phase-Two-Skill-Challenges/spec/todo_spec.rb
@@ -1,10 +1,30 @@
 require "todo"
 
 RSpec.describe "todo class" do
-  describe "#task method" do
-    it "returns a task" do
+  describe "#initialise" do
+    it "creates a new todo with the given task and sets it to not done" do
       todo = Todo.new("Cook dinner")
       expect(todo.task).to eq "Cook dinner"
+      expect(todo.done?).to eq false
+    end
+  end
+  describe "#mark_done!" do
+    it "marks the todo as done returning nothing" do
+      todo = Todo.new("Cook dinner")
+      todo.mark_done!
+      expect(todo.done?).to eq true
+    end
+  end
+  describe "#done?" do
+    it "returns true if the task is done" do
+      todo = Todo.new("Cook dinner")
+      todo.mark_done!
+      expect(todo.done?).to eq true
+    end
+    it "returns false if the task is not done" do
+      todo = Todo.new("Cook dinner")
+      expect(todo.done?).to eq false
     end
   end
 end
+
