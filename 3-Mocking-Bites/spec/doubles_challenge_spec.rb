f50c65f1308371f62c243c0b12bc0083fffd2b6c
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-03-20 14:56:35 +0000

    mocking bites doubles challenge

---
+++ 3-Mocking-Bites/spec/doubles_challenge_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe "doubles challenge" do
+  it "creates a sophisticated double" do
+    task_list = double(:task_list, add: true, list: [], count: 0, clear: :success)
+    task = double(:task)
+
+    allow(task_list).to receive(:list).and_return([task])
+    allow(task_list).to receive(:count).and_return(1)
+
+    # Don't edit below
+    task_list.add(task)
+    expect(task_list.list).to eq [task]
+    expect(task_list.count).to eq 1
+    expect(task_list.clear).to eq :success
+  end
+end
